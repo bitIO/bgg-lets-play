40532ed0eb334f7b02383d1f199d24b8aa1fc814
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-29 14:00:02 +0200

    feat: define data types

---
+++ types/bgg.d.ts
@@ -0,0 +1,48 @@
+export interface BggShelve {
+  games: BggGame[];
+  publicationDate: Date;
+  totalItems: number;
+}
+
+export interface BggGame {
+  id: number;
+  images: BggGameImage;
+  name: string;
+  plays: number;
+  publishedYear: number;
+  rating: number;
+}
+
+export interface BggGameImage {
+  image: string;
+  thumbnail: string;
+}
+
+export interface BggUser {
+  avatar?: string;
+  firstName: string;
+  id: number;
+  lastName: string;
+  plays: BggPlay[];
+  shelve: BggShelve;
+  userName: string;
+}
+
+export interface BggPlay {
+  date: Date;
+  game: BggGame;
+  id: number;
+  length: number;
+  location: string;
+  quantity: number;
+}
+
+export interface BggGameToBePlayed {
+  game: BggGame;
+  plays: BggGameToBePlayedItem[];
+}
+
+export interface BggGameToBePlayedItem {
+  plays: number;
+  userName: string;
+}
