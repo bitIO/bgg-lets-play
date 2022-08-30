891bd76cbb2f3e7bd4906f84c0b48e3cea13a317
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-30 13:17:26 +0200

    fix(api): wrap handler logic with try/catch

--- pages/api/bgg/[user]/data.ts
+++ pages/api/bgg/[user]/data.ts
@@ -4,14 +4,23 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { BggUser } from '../../../../types/bgg';
 import { loadUserData } from '../../../../utils/user';
 
-export default async (req: NextApiRequest, res: NextApiResponse<BggUser>) => {
-  const { user } = req.query;
-  if (!user) {
-    return res.status(StatusCodes.BAD_REQUEST);
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<BggUser | string>,
+) => {
+  try {
+    const { user } = req.query;
+    if (!user) {
+      return res.status(StatusCodes.BAD_REQUEST);
+    }
+    if (Array.isArray(user)) {
+      return res.status(StatusCodes.BAD_REQUEST);
+    }
+    const userData = await loadUserData(user as string);
+    return res.status(StatusCodes.OK).json(userData);
+  } catch (error) {
+    const { name, message } = error as Error;
+    console.error(`BGG-LP: ${name}: ${message}`);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(message);
   }
-  if (Array.isArray(user)) {
-    return res.status(StatusCodes.BAD_REQUEST);
-  }
-  const userData = await loadUserData(user as string);
-  return res.status(StatusCodes.OK).json(userData);
 };
--- pages/api/collection/diff.ts
+++ pages/api/collection/diff.ts
@@ -1,39 +1,49 @@
 import { StatusCodes } from 'http-status-codes';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { NotPlayedGames } from '../../../types/bgg';
 import { getNotPlayedGamesTogether } from '../../../utils/collection';
 import { loadUserData } from '../../../utils/user';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { users, sortBy } = req.query;
-  if (!users) {
-    return res.status(StatusCodes.BAD_REQUEST);
-  }
-  const userNames = (users as string).split(',');
-  if (userNames.length < 2) {
-    return res.status(StatusCodes.BAD_REQUEST);
-  }
-  const usersData = await Promise.all(
-    userNames.map((userName) => {
-      return loadUserData(userName);
-    }),
-  );
-  const notPlayedGames = getNotPlayedGamesTogether(usersData);
-  if (sortBy === 'rating') {
-    return res.json(
-      notPlayedGames.sort((a, b) => {
-        if (!a.game.stats || !b.game.stats) {
-          return 0;
-        }
-        if (a.game.stats?.rating > b.game.stats?.rating) {
-          return -1;
-        }
-        if (a.game.stats?.rating < b.game.stats?.rating) {
-          return 1;
-        }
-        return 0;
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<NotPlayedGames[] | string>,
+) => {
+  try {
+    const { users, sortBy } = req.query;
+    if (!users) {
+      return res.status(StatusCodes.BAD_REQUEST);
+    }
+    const userNames = (users as string).split(',');
+    if (userNames.length < 2) {
+      return res.status(StatusCodes.BAD_REQUEST);
+    }
+    const usersData = await Promise.all(
+      userNames.map((userName) => {
+        return loadUserData(userName);
       }),
     );
+    const notPlayedGames = getNotPlayedGamesTogether(usersData);
+    if (sortBy === 'rating') {
+      return res.json(
+        notPlayedGames.sort((a, b) => {
+          if (!a.game.stats || !b.game.stats) {
+            return 0;
+          }
+          if (a.game.stats?.rating > b.game.stats?.rating) {
+            return -1;
+          }
+          if (a.game.stats?.rating < b.game.stats?.rating) {
+            return 1;
+          }
+          return 0;
+        }),
+      );
+    }
+    return res.json(notPlayedGames);
+  } catch (error) {
+    const { name, message } = error as Error;
+    console.error(`BGG-LP: ${name}: ${message}`);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(message);
   }
-  return res.json(notPlayedGames);
 };
