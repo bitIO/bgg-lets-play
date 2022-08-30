025da99878f0833bd35ff6554d26a7a0d076e827
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-08-30 03:11:06 +0200

    feat(api): sort games not played together by rating

--- pages/api/collection/diff.ts
+++ pages/api/collection/diff.ts
@@ -5,7 +5,7 @@ import { getNotPlayedGamesTogether } from '../../../utils/collection';
 import { loadUserData } from '../../../utils/user';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const { users } = req.query;
+  const { users, sortBy } = req.query;
   if (!users) {
     return res.status(StatusCodes.BAD_REQUEST);
   }
@@ -19,5 +19,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }),
   );
   const notPlayedGames = getNotPlayedGamesTogether(usersData);
+  if (sortBy === 'rating') {
+    return res.json(
+      notPlayedGames.sort((a, b) => {
+        if (!a.game.stats || !b.game.stats) {
+          return 0;
+        }
+        if (a.game.stats?.rating > b.game.stats?.rating) {
+          return -1;
+        }
+        if (a.game.stats?.rating < b.game.stats?.rating) {
+          return 1;
+        }
+        return 0;
+      }),
+    );
+  }
   return res.json(notPlayedGames);
 };
