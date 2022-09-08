4f04d7ac66027c6fde0357eb7f28148f4bd68ba8
Francisco Calle <francisco.calle.moreno@gmail.com>
2022-09-08 17:38:06 +0200

    fix: center content in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
+import { Center, SimpleGrid } from '@mantine/core';
+
 import { UsersSelector } from '../components/UsersSelector/UsersSelector';
 import { Welcome } from '../components/Welcome/Welcome';
 
 export default function HomePage() {
   return (
-    <>
-      <Welcome />
-      <UsersSelector />
-    </>
+    <Center
+      style={{
+        height: '100vh',
+        width: '100vw',
+      }}
+    >
+      <SimpleGrid cols={1}>
+        <Welcome />
+        <UsersSelector />
+      </SimpleGrid>
+    </Center>
   );
 }
